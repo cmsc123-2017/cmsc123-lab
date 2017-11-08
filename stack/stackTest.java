5c500e684c06f2de867dcc1e055587d74aa4ced5
shaynejain <31427387+shaynejain@users.noreply.github.com>
2017-11-08 13:44:10 +0800

    Create stackTest.java

---
+++ stack/stackTest.java
@@ -0,0 +1,15 @@
+public class stackTest {
+	public void stackPush() {
+		Stack pushed = new Stack();
+		pushed.push("Football");
+		pushed.push("Basketball");
+		pushed.push("Volleyball");
+		pushed.push("Softball");
+		pushed.push("Sports");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
